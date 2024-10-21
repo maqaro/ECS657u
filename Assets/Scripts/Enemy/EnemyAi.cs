3d9030969a759d8b633ea57a7d4b5d704d7dcb5b
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2024-10-21 17:27:04 +0100

    Added Respawn script, waiting for healths script to be added

--- Assets/Scripts/Enemy/EnemyAi.cs
+++ Assets/Scripts/Enemy/EnemyAi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAiTutorial : MonoBehaviour
+public class EnemyAi : MonoBehaviour
 {
     public NavMeshAgent agent;
 
