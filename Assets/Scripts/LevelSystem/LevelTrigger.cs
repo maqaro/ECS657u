41cfea7b946249880a682fa9718cf5262d5291bb
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2024-11-01 20:30:31 +0000

    Optimised the leveloader script

--- Assets/Scripts/LevelSystem/LevelTrigger.cs
+++ Assets/Scripts/LevelSystem/LevelTrigger.cs
@@ -8,7 +8,7 @@ public class LevelTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            FindObjectOfType<LevelLoader>().LoadNextLevel();
+            FindAnyObjectByType<LevelLoader>().LoadNextLevel();
         }
     }
 }
