59bd2873228c46611caa41dc90f2595cdbf23b0b
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2024-11-30 21:40:41 +0000

    fixed the projectile so that it sticks to the target for a short while before disappearing

--- Assets/Scripts/Combat/Projectiles/Projectile.cs
+++ Assets/Scripts/Combat/Projectiles/Projectile.cs
@@ -23,25 +23,33 @@ public class Projectile : MonoBehaviour
             return;
         }
         else
+        {
             targetHit = true;
+        }
+
+        //This will stop the projectiles movemtent once it hits something
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
+
+        // Make the projectile a child of the object it hit
+        transform.SetParent(collision.transform);
+        // this will set the position of the projectile to the exact point of contact with the collider
+        transform.position = collision.contacts[0].point;
 
         // Check if the projectile hit an enemy
         if (collision.gameObject.GetComponent<TargetEnemy>() != null)
         {
             TargetEnemy enemy = collision.gameObject.GetComponent<TargetEnemy>();
-
             enemy.TakeDamage(damage);
-
-            Destroy(gameObject);
 
         }
 
         // Check if the projectile hit an extendable platform
         ExtendablePlatform platform = collision.gameObject.GetComponent<ExtendablePlatform>();
-        if (platform != null)
-        {
-            platform.ExtendPlatform();
-        }
+        platform?.ExtendPlatform();
 
         // Check if the projectile hit a platform trigger
         PlatformTrigger trigger = collision.gameObject.GetComponent<PlatformTrigger>();
