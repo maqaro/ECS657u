c7c91533614a25ff6f49ef2fd3a2afcaa90951db
Amar Younis <amaryounis0@gmail.com>
2024-12-15 13:50:13 +0000

    started dialogue box

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartDialogue(){
+        index = 0;
+    }
+
+    IEnumerator TypeLine(){
+        //Type each character one by one
+        
+    }
+}
