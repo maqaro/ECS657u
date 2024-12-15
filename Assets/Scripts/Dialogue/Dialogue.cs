4d34011fdafb2965fac2951d0cbd44446f0a93e9
Amar Younis <amaryounis0@gmail.com>
2024-12-15 14:00:42 +0000

    empty textbox

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -13,7 +13,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
     }
 
     // Update is called once per frame
@@ -24,10 +25,14 @@ public class NewBehaviourScript : MonoBehaviour
 
     void StartDialogue(){
         index = 0;
+        StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine(){
         //Type each character one by one
-        
+        foreach (char c in lines[index].ToCharArray()){
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
     }
 }
