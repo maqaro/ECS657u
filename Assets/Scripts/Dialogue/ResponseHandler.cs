b57a956cf9cdc4c7668c1f7ae4e0fab8459780d2
Amar Younis <amaryounis0@gmail.com>
2024-12-20 03:36:01 +0000

    fixed rectTransform error

--- Assets/Scripts/Dialogue/ResponseHandler.cs
+++ Assets/Scripts/Dialogue/ResponseHandler.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class ResponseHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private RectTransform responseBox;
+    [SerializeField] private RectTransform responseButtonTemplate;
+    [SerializeField] private RectTransform responseContainer;
+
+    public void ShowResponses(Response[] responses){
+        float responseBoxHeight = 0;
+
+        foreach (Response response in responses){
+            GameObject responseButton = Instantiate(responseButtonTemplate.gameObject, responseContainer);
+            responseButton.gameObject.SetActive(true);
+            responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
+            responseButton.GetComponent<Button>().onClick.AddListener(() => OnPickedResponse(response));
+
+            responseBoxHeight += responseButtonTemplate.sizeDelta.y;
+        }
+
+        responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, responseBoxHeight);
+        responseBox.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void OnPickedResponse(Response response){
         
     }
+    
 }
