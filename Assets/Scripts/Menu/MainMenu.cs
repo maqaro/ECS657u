68933cc635b46be421e71744fbbf842ca477545b
RobertHumolli <120424157+RobertHumolli@users.noreply.github.com>
2024-12-20 07:20:51 +0000

    VolumeChangerAdded

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+
     public void OnNewGameClicked()
     {
         // Reset time scale to normal
@@ -32,5 +43,38 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT FUNCTION WORKED");
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        //StartCoroutine(ConfirmationBox());
+        Debug.Log($"Volume Changed to: {AudioListener.volume.ToString("0.0")}");
+    }
+
+    //Resets the value of the Volume
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    //This is purely just to confirm the volume changing works especially as we have no sound currently
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
