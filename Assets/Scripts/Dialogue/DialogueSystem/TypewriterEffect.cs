fbc7dfd91aee1d0937bd5056b275efa7ff4e0b7c
Amar Younis <ayounis.work@gmail.com>
2024-12-22 17:40:25 +0000

    Added punctuation delay

--- Assets/Scripts/Dialogue/DialogueSystem/TypewriterEffect.cs
+++ Assets/Scripts/Dialogue/DialogueSystem/TypewriterEffect.cs
@@ -7,7 +7,14 @@ using TMPro;
 public class TypewriterEffect : MonoBehaviour
 {
 
-    [SerializeField] private float typewriterSpeed = 15f;
+    [SerializeField] private float typewriterSpeed = 17f;
+
+    private readonly Dictionary<HashSet<char>, float> punctuations = new Dictionary<HashSet<char>, float>(){
+        {new HashSet<char>{'.', '!', '?'}, 0.6f},
+        {new HashSet<char>{',', ';', ':'}, 0.3f},
+    };
+
+
     public Coroutine Run(string textToType, TMP_Text textLabel){
         return StartCoroutine(TypeText(textToType, textLabel));
     }
@@ -20,16 +27,43 @@ public class TypewriterEffect : MonoBehaviour
 
 
         while (charIndex < textToType.Length){
+            int lastCharIndex = charIndex;
+
             t += Time.deltaTime * typewriterSpeed;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
-            textLabel.text = textToType.Substring(0, charIndex);
+            for (int i = lastCharIndex; i < charIndex; i++){
+                bool isLast = i >= textToType.Length - 1;
+
+                textLabel.text = textToType.Substring(0, i + 1);
+
+                if (IsPunctuation(textToType[i], out float waitTime) && !isLast && !IsPunctuation(textToType[i + 1], out _)){
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
+
+            
             
             yield return null;
         }
 
         textLabel.text = textToType;
 
-    } 
+    }
+
+    private bool IsPunctuation(char character, out float waitTime){
+        foreach(KeyValuePair<HashSet<char>, float> punctuationCategory in punctuations){
+            if (punctuationCategory.Key.Contains(character)){
+                waitTime = punctuationCategory.Value;
+                return true;
+            }
+        }
+
+        waitTime = default;
+        return false;
+
+    }
+
+
 }
