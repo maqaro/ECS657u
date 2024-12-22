1463849e9feecf937abd03ef1b2436247fa1bdc2
Amar Younis <amaryounis0@gmail.com>
2024-12-22 02:36:50 +0000

    Applied dialogue activator to npc

--- Assets/Scripts/Dialogue/DialogueSystem/DialogueActivator.cs
+++ Assets/Scripts/Dialogue/DialogueSystem/DialogueActivator.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 public class DialogueActivator : MonoBehaviour, IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject;
+
+    private void OnTriggerEnter3D(Collider other){
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerMovement player)){
+            player.Interactable = this;
+        }
+    }
+
+    private void OnTriggerExit3D(Collider other){
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerMovement player)){
+            if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this){
+                player.Interactable = null;
+            }
+        }
+    }
+
     public void Interact(PlayerMovement player){
         player.DialogueUI.ShowDialogue(dialogueObject);
     }
