9cb7b783e722fdbe603765832a762e3a8682bc00
Amar Younis <ayounis.work@gmail.com>
2024-12-22 22:10:49 +0000

    dialogue chaining works

--- Assets/Scripts/Dialogue/DialogueSystem/DialogueActivator.cs
+++ Assets/Scripts/Dialogue/DialogueSystem/DialogueActivator.cs
@@ -5,6 +5,10 @@ public class DialogueActivator : MonoBehaviour, IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject; // The dialogue object to display when this activator is interacted with
 
+    public void UpdateDialogueObject(DialogueObject dialogueObject){
+        this.dialogueObject = dialogueObject;
+    }
+
     // Called when another collider enters the trigger zone
     private void OnTriggerEnter(Collider other)
     {
