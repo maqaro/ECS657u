ef383441c9203a339d2aa05a0a77d63ae266410f
Amar Younis <ayounis.work@gmail.com>
2024-12-24 18:03:54 +0000

    soundmanager file

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType{
+    SWORD,
+    KUNAI,
+    WALK,
+    JUMP,
+    RUN,
+    DASH,
+    GRAPPLE,
+    CLIMB,
+    WALLRUN,
+    DAMAGE,
+    DEATH
+
+}
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start(){
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1){
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
